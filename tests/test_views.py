620251028addb963d6c182b5a4fc8c1626db2e9c
Alexey Kazankov <akazankov@auchan.com>
2022-07-27 14:18:33 +0200

    Wrapping up with a solution

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,46 @@
 # tests/test_views.py
+# from flask_testing import TestCase
+# from wsgi import app
+
+# class TestViews(TestCase):
+#     def create_app(self):
+#         app.config['TESTING'] = True
+#         return app
+
+#     def test_read_many_products(self):
+#         response = self.client.get("/api/v1/products")
+#         products = response.json
+#         self.assertIsInstance(products, list)
+#         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+        
+#     def test_get_1(self):
+#         response = self.client.get("/api/v1/products/1")
+#         product = response.json
+#         self.assert_200(response, True)
+#         # self.assertEqual(response.status_code, 200)
+#         self.assertIsInstance(product, dict)
+#         self.assertEqual(product['name'], "Skello")
+        
+#     def test_get_wrong(self):
+#         response = self.client.get("/api/v1/products/5000")
+#         product = response.json
+#         self.assert_404(response, True)
+    
+#     def test_get_wrong(self):
+#         response = self.client.get("/api/v1/products/5000")
+#         product = response.json
+#         self.assert_404(response, True)
+    
+#     def del_1(self):
+#         response = self.client.delete("/api/v1/products/1")
+#         products = response.json
+#         self.assert_204(response, True)
+#         response2 = self.client.get("/api/v1/products/1")
+#         self.assert_404(response, True)
+        
+
+# SOLUTION
+
 from flask_testing import TestCase
 from wsgi import app
 
@@ -10,31 +52,111 @@ class TestViews(TestCase):
     def test_read_many_products(self):
         response = self.client.get("/api/v1/products")
         products = response.json
+        self.assertEqual(response.status_code, 200)
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
-        
-    def test_get_1(self):
+
+    def test_read_one_products(self):
         response = self.client.get("/api/v1/products/1")
         product = response.json
-        self.assert_200(response, True)
-        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertIsInstance(product, dict)
-        self.assertEqual(product['name'], "Skello")
-        
-    def test_get_wrong(self):
-        response = self.client.get("/api/v1/products/5000")
+        self.assertEqual(product['name'], 'Skello')
+
+    def test_read_one_product_not_found(self):
+        response = self.client.get("/api/v1/products/20")
         product = response.json
-        self.assert_404(response, True)
-    
-    def test_get_wrong(self):
-        response = self.client.get("/api/v1/products/5000")
+        self.assertEqual(response.status_code, 404)
+        self.assertIsNone(product)
+
+    def test_delete_product(self):
+        delete_response = self.client.delete("/api/v1/products/3")
+        deleted_product = delete_response.json
+        self.assertEqual(delete_response.status_code, 204)
+        self.assertIsNone(deleted_product)
+
+        read_one_response = self.client.get("/api/v1/products/3")
+        read_one_product = read_one_response.json
+        self.assertEqual(read_one_response.status_code, 404)
+        self.assertIsNone(read_one_product)
+
+    def test_delete_product_not_found(self):
+        response = self.client.delete("/api/v1/products/20")
         product = response.json
-        self.assert_404(response, True)
-    
-    def del_1(self):
-        response = self.client.delete("/api/v1/products/1")
-        products = response.json
-        self.assert_204(response, True)
-        response2 = self.client.get("/api/v1/products/1")
-        self.assert_404(response, True)
-        
+        self.assertEqual(response.status_code, 404)
+        self.assertIsNone(product)
+
+    def test_create_product(self):
+        response = self.client.post("/api/v1/products", json={'name': 'Netflix'})
+        product = response.json
+        self.assertEqual(response.status_code, 201)
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['name'], 'Netflix')
+
+    def test_create_product_validation_error(self):
+        response_1 = self.client.post("/api/v1/products", json={'name': 2})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 422)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.post("/api/v1/products", json={'name': ''})
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 422)
+        self.assertIsNone(product_2)
+
+    def test_create_product_bad_request(self):
+        response_1 = self.client.post("/api/v1/products", json={'other': 2})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 400)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.post("/api/v1/products", json={'other': 'what'})
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 400)
+        self.assertIsNone(product_2)
+
+        response_3 = self.client.post("/api/v1/products")
+        product_3 = response_3.json
+        self.assertEqual(response_3.status_code, 400)
+        self.assertIsNone(product_3)
+
+    def test_update_product(self):
+        update_response = self.client.patch("/api/v1/products/1", json={'name': 'Netlify'})
+        update_product = update_response.json
+        self.assertEqual(update_response.status_code, 204)
+        self.assertIsNone(update_product)
+
+        read_response = self.client.get("/api/v1/products/1")
+        product = read_response.json
+        self.assertEqual(read_response.status_code, 200)
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['name'], 'Netlify')
+
+    def test_update_product_not_found(self):
+        response = self.client.patch("/api/v1/products/20", json={'name': 'Doctolib'})
+        product = response.json
+        self.assertEqual(response.status_code, 404)
+        self.assertIsNone(product)
+
+    def test_update_product_validation_error(self):
+        response_1 = self.client.patch("/api/v1/products/1", json={'name': 2})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 422)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.patch("/api/v1/products/1", json={'name': ''})
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 422)
+        self.assertIsNone(product_2)
+
+    def test_update_product_bad_request(self):
+        response_1 = self.client.patch("/api/v1/products/1", json={'other': 'what'})
+        product_1 = response_1.json
+        self.assertEqual(response_1.status_code, 400)
+        self.assertIsNone(product_1)
+
+        response_2 = self.client.patch("/api/v1/products/1")
+        product_2 = response_2.json
+        self.assertEqual(response_2.status_code, 400)
+        self.assertIsNone(product_2)
+
